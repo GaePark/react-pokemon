3848dc5b1ffd2079f0d17e75f5a6cf77f21b6ef9
GaePark <qkrwnsgus01@gmail.com>
2024-05-22 19:48:49 +0900

    pokemon

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="react-pokemon">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
